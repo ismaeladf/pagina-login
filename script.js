9271563c006d5e8f11b255903b68cd55396e333e
Seu Nome <seuemail@example.com>
2024-10-14 12:33:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Impede o reload da página
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const messageDiv = document.getElementById("message");
+
+    // Verificação simples de usuário e senha
+    const validUsername = "ismael";
+    const validPassword = "mortadela@06";
+
+    if (username === validUsername && password === validPassword) {
+      messageDiv.innerHTML = `
+        <p style="color: green;">Login bem-sucedido!</p>
+        <a href="https://www.exemplo.com" target="_blank">Clique aqui para acessar</a>
+      `;
+    } else {
+      messageDiv.textContent = "Usuário ou senha inválidos.";
+    }
+  });
